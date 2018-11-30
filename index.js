076f4747683d30e4bdb881aed230bf3295625996
orange1337 <rosman12@gmail.com>
2018-11-30 18:15:22 +0200

    Metrics corrects

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
 
 app.use(swStats.getMiddleware({
             saveRequests: CONFIG.saveRequestsMetrics, 
+            timelineBucketDuration: 2000,
             uriPath: "/metrics",
             name : "History nodes API",
             swaggerSpec: {}
